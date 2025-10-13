ce051ca59662af455d509bf32965d1964842c80d
Krishna Pandey <krishnapandey18866@gmail.com>
2025-10-13 22:03:17 +0530

    Updated Firebase configuration and connection settings

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the Firebase scripts (only add this once in your HTML <head>)
+/*
+<script src="https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js"></script>
+*/
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAvmzU76XDNfSNgto2bcdbl6wPpRKJ9p8w",
   authDomain: "shopmatrix-23995.firebaseapp.com",
   projectId: "shopmatrix-23995",
-  storageBucket: "shopmatrix-23995.firebasestorage.app",
+  storageBucket: "shopmatrix-23995.appspot.com",
   messagingSenderId: "590775224243",
   appId: "1:590775224243:web:2bc94810a3aca673916dc6"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
