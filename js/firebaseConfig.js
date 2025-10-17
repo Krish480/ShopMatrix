c2041d46224dac3fdd2f2aa188a87dfe125a93df
Krishna Pandey <krishnapandey18866@gmail.com>
2025-10-17 20:41:57 +0530

    Change the name of firbaseconfig.js and change version of firebase to login and registration

---
+++ js/firebaseConfig.js
@@ -0,0 +1,13 @@
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAvmzU76XDNfSNgto2bcdbl6wPpRKJ9p8w",
+  authDomain: "shopmatrix-23995.firebaseapp.com",
+  projectId: "shopmatrix-23995",
+  storageBucket: "shopmatrix-23995.appspot.com",
+  messagingSenderId: "590775224243",
+  appId: "1:590775224243:web:2bc94810a3aca673916dc6"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
